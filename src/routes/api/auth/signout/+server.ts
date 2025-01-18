ee2e5a38a71afb3b8f7a0f698e9a307924ec6371
Jonathan Lo <jonathanlo411@gmail.com>
2025-01-17 23:39:09 -0800

    fix: fix signout on mobile devices by invalidating cookie before hook

--- src/routes/api/auth/signout/+server.ts
+++ src/routes/api/auth/signout/+server.ts
@@ -1,47 +1,25 @@
+import { invalidateSession } from "$lib/server/auth";
+import { sha256 } from "@oslojs/crypto/sha2";
+import { encodeHexLowerCase } from "@oslojs/encoding";
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ cookies, locals }) => {
-  try {
-      // Remove from database
-      const sessionId = cookies.get('sessionId');
-      if (sessionId) {
-          await prisma.session.delete({
-              where: { id: sessionId }
-          });
+    try {
+      const sessionToken = cookies.get('session');
+      if (sessionToken) {
+        const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(sessionToken)));
+        await invalidateSession(sessionId);
       }
-
-      // Clear all cookies
-      const allCookies = cookies.getAll();
-      allCookies.forEach(cookie => {
-          cookies.delete(cookie.name, { 
-              path: '/',
-              domain: undefined,
-              secure: true,
-              httpOnly: true,
-              sameSite: 'strict'
-          });
-      });
-
-      // Reset locals
-      locals.user = {
-          id: '',
-          email: '',
-          firstName: null,
-          lastName: null,
-          role: 'USER'
-      };
-
+  
       const headers = new Headers();
-      headers.set('Location', '/');
-      headers.append('Set-Cookie', 'sessionId=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict');
-      headers.append('Set-Cookie', 'auth=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict');
+      headers.append('Set-Cookie', 'session=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure; SameSite=Strict');
       
       return new Response(null, {
-          status: 303,
-          headers
+        status: 303,
+        headers
       });
-  } catch (err) {
+    } catch (err) {
       console.error('Error in signout:', err);
       throw redirect(303, '/');
-  }
-};
+    }
+  };
